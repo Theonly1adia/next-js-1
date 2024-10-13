63bbbc7ee9e289bc332b6a3e8373c307ac9c0f93
Adia Holt <adia.holt@gmail.com>
2024-10-13 16:19:34 -0500

    save changes for index.js

---
+++ src/pages/app/about/index.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+      <div>
+        <h1>This is all about me!</h1>
+        <p>I am learning Next.js, and this is my first custom page.</p>
+      </div>
+    );
+  }
+  

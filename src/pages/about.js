551310bf3d4cc140e37d926362cf8de6aa4ab8d3
Adia Holt <adia.holt@gmail.com>
2024-10-13 17:00:38 -0500

    change styling of picture

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,8 +1,17 @@
 export default function About() {
     return (
-      <div>
+      <div style={{ textAlign: 'center', padding: '20px' }}>
         <h1>This is all about me!</h1>
         <p>I am learning Next.js, and this is my first custom page.</p>
+        <br></br>
+        
+        <img 
+          src="/images/adia.jpg" 
+          alt="A picture of Adia" 
+          style={{ width: '200px', 
+            borderRadius: '55%',
+            marginTop: '20px' }} 
+        />
       </div>
     );
   }
